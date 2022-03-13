3f295ccd6bcca9803029c8030a26f41b9e9dcac5
Richard Espinoza <omivalera@gmail.com>
2022-03-13 14:19:55 -0300

    error en api al cargar busqueda

--- store/urls.py
+++ store/urls.py
@@ -7,27 +7,26 @@ from .views import ProductList
 router = routers.DefaultRouter()
 
 # All products, no filter
-
-
+# 12 products per page
 router.register(r'products', views.ProductViewSet)
 
 # Products grouped by category, no filter
-# 1 categoris por pagina
+# 1 category per page
 router.register(r'groups', views.GroupViewSet)
 
-# Todas las cateogiras
+# All categories of products
 router.register(r'categories', views.CategoryViewSet)
-router.register(r'list', views.ProductViewSet)
-
-
-
 
 urlpatterns = [
     path('', include(router.urls)),
     
-
+    # 12 products per page
+    # Regex search
     path('list/', ProductList.as_view(), name='postsearch'),
-
+    # + Filter by category
+    # + Order by category id, name, price
+    # Example of use: 
+    # http://hosturl/list/1/?page=1&search=&ordering=category_id
     
     path('list/<int:category_id>/', ProductList.as_view()),
 ]
